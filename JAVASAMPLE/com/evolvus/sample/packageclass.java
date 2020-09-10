ec4b67a4971d8c2bf79e14106d1368932e66f60c
NagendrappaE-evolvus <nagendrappa.e@evolvussolutions.com>
2020-09-10 11:10:20 +0530

    F

---
+++ JAVASAMPLE/com/evolvus/sample/packageclass.java
@@ -0,0 +1,12 @@
+package com.evolvus.sample;
+
+public class packageclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+        System.out.println( "Hello World!" );
+
+	}
+
+}
